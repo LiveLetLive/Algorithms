6972ed5aa479e7461463c9b265f1ed99da18e29b
LiveLetLive <LiveLetLive@users.noreply.github.com>
2016-09-17 07:51:58 +0530

    Add files via upload

--- Algorithms/src/uva/UVA_812_Trade_on_Verweggistan.cpp
+++ Algorithms/src/uva/UVA_812_Trade_on_Verweggistan.cpp
@@ -29,6 +29,7 @@ int maxprofit[MAXW];
 int cntIdx[MAXW];
 int minLen[MAXW*MAXB];
 int maxprofitIndex[MAXW][MAXB];
+int maxprofitMinSet[MAXW][MAXB];
 int maxprofitAns = 0;
 
 
@@ -39,7 +40,7 @@ void reset()
 		maxprofit[i] = 0;
 		cntIdx[i] = 0;
 		FOR(j, 0, MAXB)
-			maxprofitIndex[i][j] = 0, profit[i][j] = 0;
+			maxprofitIndex[i][j] = 0, profit[i][j] = 0, maxprofitMinSet[i][j] = 0;
 	}
 
 	maxprofitAns = 0;
@@ -48,6 +49,7 @@ void reset()
 		minLen[i] = 0;
 }
 
+//Iterate thru all combinations & find the min
 void FindMinLen(int idx, int sum)
 {
 	if(idx == W) 
@@ -98,25 +100,62 @@ int main()
 				maxprofit[i] = profit[i][j];
 		}
 
+
+		//Find max profit with sorted indexes
 		FOR(i, 0, W)
 		{
 			if(maxprofit[i] == 0)
 				maxprofitIndex[i][cntIdx[i]++] = 0;
-			
+
 			FOR(j, 0, Bcnt[i]) 
 				if( (maxprofit[i] == profit[i][j]))
 					maxprofitIndex[i][cntIdx[i]++] = j+1;
 		}
 
+
+		int minLenT = INF;
+		int minI = INF;
+		int minJ = INF;
+		int idx[MAXW] = { 1, };
+
+		//Find the min of first row
+		FOR(i, 0, W)
+			minLenT = min(minLenT, maxprofitIndex[i][0]), idx[i] = 1;
+
+		//select only 10 values more than min in first row
+		FOR(k, 0, 10)
+			FOR(i, 0, W)
+		{
+			int j = 0;
+
+			for(j = idx[i]; j < cntIdx[i]; j++)
+				if(minLenT < maxprofitIndex[i][j])
+				{
+					minI = i, minJ = j;
+					idx[minI] = minJ + 1;
+					break;
+				}
+				if(j != cntIdx[i])
+					break;
+		}
+
+		//Prune the cnt, to reduce the load during backtracking.
+		FOR(i, 0, W)
+			if(idx[i] < cntIdx[i])
+				cntIdx[i] = idx[i];
+		
+		//Find Max answer 
 		FOR(i, 0, W)
 			maxprofitAns += maxprofit[i];
 
+		//Run combinations
 		FindMinLen(0, 0);
 
 		printf("Workyards %d\n", t++);
 		printf("Maximum profit is %d.\n", maxprofitAns);
 		printf("Number of pruls to buy:");
 
+		//Print only 10 values
 		int count = 0;
 		FOR(i, 0, MAXW*MAXB)
 		{
