5ba5cc5f4cf837ad58dd037b01ea13504eb7cb08
LiveLetLive <LiveLetLive@users.noreply.github.com>
2016-11-01 21:50:32 +0530

    Add files via upload

---
+++ Algorithms/src/uva/UVA_1057_Routing.cpp
@@ -0,0 +1,159 @@
+#include "CommonHeader.h"
+
+#ifdef UVA_1057_ROUTING
+READ_INPUT(UVA_1057_ROUTING)
+	/*
+	#include <iostream>
+	#include <algorithm>
+	#include <string>
+	#include <string.h>
+	#include <math.h>
+	#include <float.h>
+	#include <queue>
+	#include <map>
+	#include <stdio.h>
+
+	using namespace std;
+
+	#define FOR(i, init, cnt) for(int i = init; i < cnt; i++)
+	#define MAXN 1003
+	#define INF 0x3f3f3f3fL
+	typedef long long ll;
+
+	int og[MAXN][MAXN];
+	int g[MAXN][MAXN];
+	int N, E;
+
+	void reset()
+	{
+	FOR(i, 0, MAXN)
+	FOR(j, 0, MAXN)
+	g[i][j] = 0;
+	}
+
+	int main()
+	{
+	int T;
+	while(scanf("%d %d ", &N, &E), N, E)
+	{
+	FOR(i, 0, E)
+	{
+	int x, y;
+	scanf("%d %d ", &x, &y);
+	og[x][y] = 1;
+	g[x][y] = 1;
+	}
+
+	FOR(k, 0, N+1)
+	FOR(i, 0, N+1)
+	FOR(j, 0, N+1)
+	g[i][j] = min(g[i][j], g[i][k]+g[k][j]);
+
+	dijkstra();
+	}
+	return 0;
+	}*/
+
+#include <stdio.h> 
+#include <string.h> 
+#include <queue> 
+#include <algorithm> 
+	using namespace std; 
+int g [105] [105]; 
+int dp [105] [105], inq [105] [105]; 
+
+int main () 
+{ 
+	int n, m; 
+	int i, j, k; 
+	int x, y, cost; 
+	int cases = 0; 
+	while (scanf ("%d %d ", &n, &m) == 2 && n) 
+	{ 
+		memset (inq, 0, sizeof (inq)); 
+		for (i = 1; i <= n; i ++) 
+		{ 
+			for (j = 1; j <= n; j ++) 
+			{ 
+				g [i] [j] = 0xfffffff; 
+			} 
+		} 
+		for (i = 0; i <m; i ++) 
+		{ 
+			scanf ( "%d %d ", &x, &y); 
+			g[x][y] = 1; 
+		} 
+
+		for (k = 1; k <= n; k ++) 
+			for (i = 1; i <= n; i ++) 
+				for (j = 1; j <= n; j ++) 
+					g[i][j] = min(g[i][j], g[i][k] + g[k][j]); 
+
+		for (i = 1; i <= n; i ++) 
+			for (j = 1; j <= n; j ++) 
+				dp[i][j] = 0xfffffff; 
+
+		queue <int> X, Y; 
+		X.push(1), Y.push(1); 
+
+		dp[1][1] = 1; 
+		while (!X.empty()) 
+		{ 
+			x = X.front(), X.pop(); 
+			y = Y.front(), Y.pop(); 
+			inq [x] [y] = 0; 
+			// printf ( "% 2d% 2d% d \ n", x, y, dp [x] [y]); 
+			for (i = 1; i <= n; i ++) 
+			{
+				cost = (x != i && y != i); 
+				if (g [x][i] == 1) 
+				{ 
+					if (dp [i][y] > dp [x][y] + cost) 
+					{ 
+						dp[i][y] = dp[x][y] + cost; 
+						if (inq [i] [y] == 0) 
+						{ 
+							inq [i] [y] = 1; 
+							X.push (i), Y.push (y); 
+						} 
+					} 
+				} 
+				if (g [i] [y] == 1) 
+				{ 
+					if (dp [x] [i]> dp [x] [y] + cost) 
+					{ 
+						dp [x] [i] = dp [x] [y] + cost; 
+						if (inq [x] [i] == 0) 
+						{ 
+							inq [x] [i] = 1; 
+							X.push (x), Y.push (i); 
+						} 
+					} 
+				} 
+			} 
+			if (x != y) 
+			{ 
+				if (dp[y][x] > dp [x] [y] + g [x] [y] -1) 
+				{
+					dp [y] [x] = dp [x] [y] + g [x] [y] -1; 
+					if (inq [y] [x] == 0) { 
+						inq [y] [x] = 1; 
+						X.push (y), Y.push (x); 
+					}
+				}
+			} 
+		} 
+
+		printf("Network %d\n", ++cases); 
+		if (dp[2][2] == 0xfffffff) 
+			puts ("Impossible"); 
+		else 
+			printf("Minimum number of nodes = %d \n", dp [2] [2]); 
+		puts(""); 
+	} 
+	return 0; 
+}
+
+
+
+#endif
---
+++ Algorithms/src/uva/UVA_928_Eternal_Truths.cpp
@@ -0,0 +1,143 @@
+#include "CommonHeader.h"
+
+#ifdef UVA_928_ETERNAL_TRUTHS
+READ_INPUT(UVA_928_ETERNAL_TRUTHS)
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <math.h>
+#include <float.h>
+#include <queue>
+#include <map>
+#include <stdio.h>
+
+	using namespace std;
+
+#define FOR(i, init, cnt) for(int i = init; i < cnt; i++)
+#define MAXN 303
+#define INF 0x3f3f3f3fL
+typedef long long ll;
+
+int R, C;
+int si, sj, ei, ej;
+int g[MAXN][MAXN];
+int dp[MAXN][MAXN][4][3];
+bool vis[MAXN][MAXN][4][3];
+int totalSteps;
+int stepCnt;
+
+void reset()
+{
+	totalSteps = INF;
+	stepCnt = 0;
+	FOR(i, 0, MAXN)
+		FOR(j, 0, MAXN)
+		g[i][j] = 0;
+
+	FOR(i, 0, MAXN)
+		FOR(j, 0, MAXN)
+		FOR(k, 0, 4)
+		FOR(l, 0, 3)
+		dp[i][j][k][l] = INF, vis[i][j][k][l] = 0;
+
+}
+
+int dr[] = { 0,  0, 1, -1 };
+int dc[] = { 1, -1, 0, 0 };
+
+inline bool isValid(int i , int j)
+{
+	if(i < 0 || j < 0 || i >= R || j >= C) return false;
+	if(g[i][j] == 0) return false;
+
+	return true;
+}
+
+int solve(int i, int j, int dir, int step)
+{
+	if(i == ei && j == ej)
+	{
+		if(stepCnt < totalSteps)
+			totalSteps = stepCnt;
+		return stepCnt;
+	}
+
+	if(stepCnt > totalSteps)
+		return INF;
+
+	if(dp[i][j][dir][step] < INF)
+		return dp[i][j][dir][step];
+
+	int ans = INF;
+
+	FOR(x, 0, 4)
+	{
+		int nStep = (((step) % 3) + 1);
+		int ni = i + dr[x] * nStep;
+		int nj = j + dc[x] * nStep;
+
+		if(isValid(ni, nj))
+		{
+			if(nStep >= 2)
+			{
+				int ni1 = i + dr[dir] * (nStep-1);
+				int nj1 = j + dc[dir] * (nStep-1);
+
+				if(g[ni1][nj1] == 0) continue;
+
+				if(nStep == 3)
+				{
+					int ni1 = i + dr[dir] * (nStep-2);
+					int nj1 = j + dc[dir] * (nStep-2);
+
+					if(g[ni1][nj1] == 0) continue;
+				}
+			}
+
+			if(vis[ni][nj][x][nStep]) continue;
+
+			vis[ni][nj][x][nStep] = 1;
+			stepCnt++;
+			ans = min(ans, solve(ni, nj, x, nStep)); 
+			vis[ni][nj][x][nStep] = 0;
+			stepCnt--;
+		}
+	}
+
+	return dp[i][j][dir][step] = ans;
+}
+
+
+int main()
+{
+	int T;
+	scanf("%d ", &T);
+	FOR(t, 0, T)
+	{
+		reset();
+		scanf("%d %d ", &R, &C);
+		FOR(i, 0, R)
+		{
+			char str[MAXN] = { 0 };
+			scanf("%s ", &str);
+			FOR(j, 0, C)
+			{
+				if(str[j] == 'S') si = i, sj = j, g[i][j] = 1;
+				else if(str[j] == 'E') ei = i, ej = j, g[i][j] = 1;
+				else if(str[j] == '#') g[i][j] = 0;
+				else if(str[j] == '.') g[i][j] = 1;
+			}
+		}
+
+		int ans = solve(si, sj, 0, 0);
+		if(totalSteps >= INF)
+			printf("NO\n");
+		else
+			printf("%d\n", totalSteps);
+	}
+	return 0;
+}
+
+#endif
