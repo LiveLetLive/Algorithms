82438a9456eb5fa90499e588bb8d04edcce4fe46
LiveLetLive <LiveLetLive@users.noreply.github.com>
2017-05-09 11:26:26 +0530

    Add files via upload

--- Algorithms/src/uva/Self_Find_Largest_Farm.cpp
+++ Algorithms/src/uva/Self_Find_Largest_Farm.cpp
@@ -4,40 +4,28 @@
 READ_INPUT(SELF_FIND_BIGGEST_RECTANGLE)
 
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <string.h>
-#include <math.h>
-#include <float.h>
-#include <queue>
-#include <map>
 #include <stdio.h>
 
 	using namespace std;
 
 #define FOR(i, init, cnt) for(int i = init; i < cnt; i++)
 #define MAXN 16
-#define MAXL 51
-#define INF 0x3f3f3f3fL
-typedef long long ll;
-typedef short us;
+#define MAXL 41
+
+typedef int us;
 int N;
 us s[MAXN];
-us sum[1<<MAXN];
 
-//#define USE_HEAP
-
-#ifdef USE_HEAP
 class DP
 {
 public:
-	short data[MAXN][MAXL][MAXL][MAXL];
+	short data[MAXN][MAXL][MAXL][MAXL][MAXL];
 };
 
 DP *dp = NULL;
-#else
-short dp[MAXN][MAXL][MAXL][MAXL];
-#endif
+
+#define Mymax(a,b)            (((a) > (b)) ? (a) : (b))
+
 
 void reset()
 {
@@ -45,20 +33,12 @@ void reset()
 		FOR(j, 0, MAXL)
 		FOR(k, 0, MAXL)
 		FOR(m, 0, MAXL)
-#ifdef USE_HEAP
-		dp->data[i][j][k][m] = 0;
-#else
-		dp[i][j][k][m] = 0;
-#endif
-
-	FOR(i, 0, 1 << N)
-		sum[i] = 0;
+		FOR(n, 0, MAXL)
+		dp->data[i][j][k][m][n] = 0;
 }
 
-us solve(us idx, us l1, us l2, us w1, int bitmask)
+us solve(us idx, us l1, us l2, us w1, us w2)
 {
-	us w2 = sum[bitmask] - (l1 + l2 + w1);
-
 	if(idx == N)
 	{
 		if(l1 && w1 && l1 == l2 && w1 == w2)
@@ -70,35 +50,51 @@ us solve(us idx, us l1, us l2, us w1, int bitmask)
 	if(l1 >= MAXL || l2 >= MAXL || w1 >= MAXL || w2 >= MAXL)
 		return -1;
 
-#ifdef USE_HEAP
-	if(dp->data[idx][l1][l2][w1] != 0)
-		return dp->data[idx][l1][l2][w1];
-#else
-	if(dp[idx][l1][l2][w1] != 0)
-		return dp[idx][l1][l2][w1];
-#endif
+	if(dp->data[idx][l1][l2][w1][w2] != 0)
+		return dp->data[idx][l1][l2][w1][w2];
+
+	if(dp->data[idx][l1][l2][w2][w1] != 0)
+		return dp->data[idx][l1][l2][w2][w1];
+
+	if(dp->data[idx][l2][l1][w1][w2] != 0)
+		return dp->data[idx][l2][l1][w1][w2];
+
+	if(dp->data[idx][l2][l1][w2][w1] != 0)
+		return dp->data[idx][l2][l1][w2][w1];
+
+	if(dp->data[idx][w1][w2][l1][l2] != 0)
+		return dp->data[idx][w1][w2][l1][l2];
+
+	if(dp->data[idx][w1][w2][l2][l1] != 0)
+		return dp->data[idx][w1][w2][l2][l1];
+
+	if(dp->data[idx][w2][w1][l2][l1] != 0)
+		return dp->data[idx][w2][w1][l2][l1];
+
+	if(dp->data[idx][w2][w1][l1][l2] != 0)
+		return dp->data[idx][w2][w1][l1][l2];
 
 	us ans = -1;
 
-	ans = max(ans, solve(idx+1, l1 + s[idx],	l2,				w1,				bitmask | (1 << idx)));
-	ans = max(ans, solve(idx+1, l1 ,			l2 + s[idx],	w1,				bitmask | (1 << idx)));
-	ans = max(ans, solve(idx+1, l1 ,			l2,				w1 + s[idx],	bitmask | (1 << idx)));
-	ans = max(ans, solve(idx+1, l1 ,			l2,				w1,				bitmask | (1 << idx)));
-	ans = max(ans, solve(idx+1, l1 ,			l2,				w1,				bitmask));
+	int r1 = solve(idx+1, l1 + s[idx],	l2,				w1,				w2);
+	int r2 = solve(idx+1, l1 ,			l2 + s[idx],	w1,				w2);
+	int r3 = solve(idx+1, l1 ,			l2,				w1 + s[idx],	w2);
+	int r4 = solve(idx+1, l1 ,			l2,				w1,				w2 + s[idx]);
+	int r5 = solve(idx+1, l1 ,			l2,				w1,				w2);
 
-#ifdef USE_HEAP
-	return dp->data[idx][l1][l2][w1] = ans;
-#else
-	return dp[idx][l1][l2][w1] = ans;
-#endif
+	ans = Mymax(ans, r1);
+	ans = Mymax(ans, r2);
+	ans = Mymax(ans, r3);
+	ans = Mymax(ans, r4);
+	ans = Mymax(ans, r5);
+
+	return dp->data[idx][l1][l2][w1][w2] = ans;
 }
 
 int main()
 {
 	int T;
-#ifdef USE_HEAP
 	dp = new DP;
-#endif
 	scanf("%d ", &T);
 	FOR(t, 0, T)
 	{
@@ -107,20 +103,12 @@ int main()
 
 		FOR(i, 0, N)
 			scanf("%d ", &s[i]);
-
-		FOR(i, 0, 1 << N)
-			FOR(j, 0, N)
-			if(i & (1<<j))
-				sum[i] += s[j];
-
-		//if(t > 10) continue;
-
-		printf("%d\n", solve(0, 0, 0, 0, 0));
+		
+		int ans = solve(0, 0, 0, 0, 0);
+		printf("%d\n", ans);
 	}
 
-#ifdef USE_HEAP
-	//delete dp;
-#endif
+	delete dp;
 	return 0;
 }
 
