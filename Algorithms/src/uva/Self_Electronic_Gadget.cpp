7373da5c73e855a062026c27410ed36246543d4b
LiveLetLive <LiveLetLive@users.noreply.github.com>
2017-06-15 07:18:02 +0530

    Add files via upload

--- Algorithms/src/uva/Self_Electronic_Gadget.cpp
+++ Algorithms/src/uva/Self_Electronic_Gadget.cpp
@@ -20,84 +20,176 @@ READ_INPUT(SELF_ELECTRONIC_GADGET)
 #define INF 0x3f3f3f3fL
 typedef long long ll;
 
-// 1 denotes , not usable
-int refTable[MAXN];
-int activeTable[MAXN];
 
-void GenTable()
+int b[10];
+int bcnt = 0;
+int w[10];
+int wcnt = 0;
+
+int sw[10][4];
+int currTryIdx = 0;
+int currTryVal = 1;
+int stage = 0;
+int oldInput[4];
+
+
+typedef struct
 {
-	FOR(i, 0, MAXN)
+	int har;
+	int iso;
+}MatchRes;
+
+typedef struct Result
+{
+	int in[4];
+	MatchRes m;
+}Res;
+
+Res steps[100];
+int stepIdx;
+
+void PrintDebug()
+{
+	FOR(i, 0, stepIdx)
 	{
-		int visited[10] = { 0 };
-		int repeat = 0;
-
-		int num = i;
-		int count = 4;
-
-		while(count--)
-		{
-			if(visited[num % 10])
-			{
-				repeat = 1;
-				break;
-			}
-			visited[num % 10] = 1;
-			num /= 10;
-		}
-
-		if(repeat == 1)
-			refTable[i] = 1;
+		printf("{ ");
+		FOR(j, 0, 4)
+			printf("%d, ", steps[i].in[j]);
+		printf("[%d, %d]", steps[i].m.har, steps[i].m.iso);
+		printf(" }\n");
 	}
 }
 
-
-//idx is index from right (right most == 0)
-void UpdateRefTable(int idx, int Foundnum)
+void init()
 {
-	FOR(i, 0, MAXN)
+	FOR(i, 0, 100)
 	{
-		int visited[10] = { 0 };
-		int repeat = 0;
+		steps[i].m.har = 0;
+		steps[i].m.iso = 0;
+		FOR(j, 0, 4)
+			steps[i].in[j] = -1;
+	}
 
-		int num = i;
-		int count = 4;
-		visited[Foundnum] = 1;
+	stepIdx = 0;
 
-		while(count--)
+	FOR(i, 0, 10)
+	{
+		b[i] = -1, w[i] = -1, oldInput[i] = -1;
+		FOR(j, 0, 4)
+			sw[i][j] = 1;
+	}
+	currTryIdx = 0;
+	currTryVal = 1;
+	stage = 0;
+	bcnt = 0;
+	wcnt = 0;
+}
+
+MatchRes match(int inputD[])
+{
+	MatchRes r = {0};
+	return r;
+}
+
+void UpdateStep(int in[], MatchRes m)
+{
+	int idx = stepIdx++;
+	steps[idx].m.har = m.har;
+	steps[idx].m.iso = m.iso;
+	FOR(j, 0, 4)
+		steps[idx].in[j] = in[j];
+}
+
+void HarIsoTrivialCheck(MatchRes n, int in[])
+{
+	if(n.har == 0)
+		FOR(i, 0, 4)
+		sw[in[i]][i] = 0;
+	else if(n.har == 4)
+		FOR(i, 0, 4)
+		w[wcnt++] = in[i];
+
+	if(n.iso == 0)
+		FOR(i, 0, 4)
+		b[bcnt++] = in[i];
+	else if(n.iso == 4)
+		FOR(i, 0, 4)
+		w[wcnt++] = in[i];
+}
+
+void UpdateAllDB(int in[], int oldin[], MatchRes n, MatchRes o)
+{
+	//Just update database without decision
+	if(oldin == NULL)
+	{
+
+		UpdateStep(in, n);
+	}
+
+	//Make decision 
+}
+
+int getStatus(MatchRes n)
+{
+	if(n.har == 4)
+		return 2;
+	if(n.iso == 4)
+		return 1;
+	return 0;
+}
+
+void solve(int inputD[])
+{
+	init();
+
+	inputD[0] = 0;
+	inputD[1] = 7;
+	inputD[2] = 8;
+	inputD[3] = 9;
+
+	MatchRes or = match(inputD);
+
+	int ret = getStatus(or);
+	if(ret == 2) return;
+	else if(ret == 1) stage = 1;
+	
+	UpdateAllDB(inputD, NULL, or, or);
+
+	MatchRes r = { 0, 0};
+
+	while(1)
+	{
+		//Finding whitelist
+		if(stage == 0)
 		{
-			if(Foundnum == (num % 10) && count == idx)
-				continue;
+			r = match(inputD);
+			int ret = getStatus(r);
+			if(ret == 2) return;
+			else if(ret == 1) stage = 1;
+		
+			UpdateAllDB(inputD, oldInput, r, or);
 
-			if(visited[num % 10])
-			{
-				refTable[i] = 1;
-			}
-			num /= 10;
+			or = r;
+			FOR(i, 0, 4) oldInput[i] = inputD[i];
+			continue;
 		}
+		// Finding indexes
+		else
+		{
+		}
+		//update all b, w, sw
+
 	}
 }
 
-int GiveFirstZero()
-{
-	FOR(i, 0, MAXN)
-		if(refTable[i] == 0)
-			return i;
-}
 
 int main()
 {
-	GenTable();
 	int T;
 	scanf("%d ", &T);
 	FOR(t, 0, T)
 	{
-		FOR(i, 0, MAXN)
-			activeTable[i] = refTable[i];
 
-		int fguess = GiveFirstZero();
-
-		printf("%d", fguess);
-		
 
 	}
 	return 0;
