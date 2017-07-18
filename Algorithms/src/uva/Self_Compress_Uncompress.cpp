1531e48cf8fd5d123fca8d89af9ba2e5f72a527f
LiveLetLive <LiveLetLive@users.noreply.github.com>
2017-07-18 21:13:24 +0530

    Add files via upload

--- Algorithms/src/uva/Self_Compress_Uncompress.cpp
+++ Algorithms/src/uva/Self_Compress_Uncompress.cpp
@@ -4,27 +4,14 @@
 READ_INPUT(SELF_COMPRESS_UNCOMPRESS)
 
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <string.h>
-#include <math.h>
-#include <float.h>
-#include <queue>
-#include <map>
 #include <stdio.h>
 
 	using namespace std;
 
 #define FOR(i, init, cnt) for(int i = init; i < cnt; i++)
-#define MAXN 1003
-#define INF 0x3f3f3f3fL
-typedef long long ll;
-
 
 #include<stdio.h> 
 #include<stdlib.h>
-
-int N;
 
 #define T 50
 #define MAXVAL 999996 
@@ -34,14 +21,16 @@ static char TC[T][MAXVAL];
 static int OP[T][MAXS]; 
 static char ANS[T][MAXVAL]; 
 static int length[T];
+static int Anslength[T];
+static int N;
 
 static char c[31]  = {
 	'a','b','c','d','e','f','g','h','i','j','k','l','m','n', 'o','p','q','r','s','t','u','v','w','x','y','z','+','-','/','*',' '
 };
 
 
-extern void compress(const char *input, int inputLength, int *output, int outputLength); 
-extern void decompress(const int *input, int inputLength, char *output, int outputLength); 
+extern void compress(const char *input, int inputLength, int *output); 
+extern void decompress(const int *input, char *output, int *outputLength); 
 extern void Init();
 
 
@@ -54,39 +43,27 @@ void generateTC()
 			TC[0][j] = c[rand()%31];
 	}
 
-	for(int i = 1; i < T;i++)
+	for(int i = 1; i < T-1;i++)
 	{
 		N = 500000 + rand() % 500000; 
-		N = N - N%6;
+		N = N - N % 6;
 
 		length[i] = N; 
-		for(int j=0;j<N;j++)
+		for(int j = 0; j < N; j++)
 			TC[i][j] = c[rand() % 31];
+	}
+
+	{
+		N = 20;
+		length[T-1] = N;
+		for(int j = 0;j < N;j++)
+			TC[T-1][j] = c[rand()%31];
 	}
 }
 
 
 int tcorder[T] = {0,}; 
 int visited[T] = {0,};
-int op[50];
-
-
-//int main()
-//{
-//	char str[50] = {0,};
-//	char str2[50] = {0,};
-//
-//	strncpy(str, c, 31);
-//	str[31] = 0;
-//
-//	printf("%s\n", str);
-//	compress(str, 31, op, 50);
-//	decompress(op, 50, str2, 50);
-//	str2[49] = 0;
-//
-//	printf("%s\n", str2);
-//
-//}
 
 int main()
 {
@@ -101,7 +78,6 @@ int main()
 			tcorder[t] = i; 
 			visited[i] = 1; 
 			t++;
-
 		}
 	}
 
@@ -109,14 +85,22 @@ int main()
 	Init();
 
 	for(i = 0;i < T; i++)
-		compress(TC[tcorder[i]], length[tcorder[i]], OP[tcorder[i]], MAXS);
+		compress(TC[tcorder[i]], length[tcorder[i]], OP[tcorder[i]]);
 
 	for(i=0;i<T;i++)
-		decompress(OP[i], MAXS, ANS[i], MAXVAL);
+		decompress(OP[i], ANS[i], &Anslength[i]);
 
 	for(i = 0; i < T; i++) 
 	{
 		int lim = length[i]; 
+
+		if(length[i] != Anslength[i])
+		{
+			printf("0\n"); 
+			continue;
+		}
+
+
 		for(j = 0;j < lim;j++) 
 		{
 			if(TC[i][j] != ANS[i][j])
@@ -136,16 +120,16 @@ int main()
 
 struct Header
 {
-	long long len:20;
-
-	long long a1:5;
-	long long a2:5;
-	long long a3:5;
-	long long a4:5;
-	long long a5:5;
-	long long a6:5;
-	long long a7:5;
-	long long a8:5;
+	struct Stuff
+	{
+		unsigned int len:2;
+		unsigned int a11:5;
+		unsigned int a12:5;
+		unsigned int a13:5;
+		unsigned int a14:5;
+		unsigned int a15:5;
+		unsigned int a16:5;
+	} TenSuff[10];
 };
 
 void MemCpy(void* dest, void* src, int n)
@@ -181,76 +165,93 @@ int GetIndex(char c)
 	FOR(i, 0, CountOf(sp))
 		if(c == sp[i].c)
 			return 'z' - 'a' + sp[i].idx;
+
+	return 0;
 }
 
 char GetChar(int idx)
 {
 	idx &= 0x1F;
 	if(idx >= 'a' - 'a' && idx <= 'z' - 'a')
-		return idx + 'a';
+		return (char)(idx + 'a');
 
 	int sIdx = idx - ('z' - 'a');
 
 	FOR(i, 0, CountOf(sp))
 		if(sIdx == sp[i].idx)
 			return sp[i].c;
+
+	return 0;
 }
 
 
-void compress(const char *input, int inputLength, int *output, int outputLength)
+void compress(const char *input, int inputLength, int *output)
 {
 	struct Header h = { 0,};
+	int sizeOfHeader = sizeof(h);
 
-	h.len = inputLength;
-	h.a1 = GetIndex(input[0]);
-	h.a2 = GetIndex(input[1]);
-	h.a3 = GetIndex(input[2]);
-	h.a4 = GetIndex(input[3]);
-	h.a5 = GetIndex(input[4]);
-	h.a6 = GetIndex(input[5]);
-	h.a7 = GetIndex(input[6]);
-	h.a8 = GetIndex(input[7]);
+	FOR(i, 0, 10)
+	{
+		h.TenSuff[i].len = (inputLength & (0x3 << (i*2))) >> (i*2);
+		h.TenSuff[i].a11 = GetIndex(input[6 * i + 0]);
+		h.TenSuff[i].a12 = GetIndex(input[6 * i + 1]);
+		h.TenSuff[i].a13 = GetIndex(input[6 * i + 2]);
+		h.TenSuff[i].a14 = GetIndex(input[6 * i + 3]);
+		h.TenSuff[i].a15 = GetIndex(input[6 * i + 4]);
+		h.TenSuff[i].a16 = GetIndex(input[6 * i + 5]);
+	}
 
-	MemCpy(output, &h, sizeof(long long));
+	MemCpy(output, &h, sizeOfHeader);
 
-	int *ptr = (int*)((char*)output +sizeof(long long));
+	int *ptr = (int*)((char*)output + sizeOfHeader);
 
-	for(int i = 0; i < inputLength - 8; )
+	for(int i = 0; i < inputLength - 60; )
 	{
 		int k = i/6;
 		ptr[k] = 0;
 
-		for(int j = 0; j < 6 && i < (inputLength - 8); ++j) 
-			ptr[k] |= (GetIndex(input[i+8]) << (5 * j)), ++i;
+		for(int j = 0; j < 6 && i < (inputLength - 60); ++j) 
+			ptr[k] |= (GetIndex(input[i+60]) << (5 * j)), ++i;
 	}
 }
 
-void decompress(const int *input, int inputLength, char *output, int outputLength)
+void decompress(const int *input, char *output, int *outputLength)
 {
-	long long l = *((long long*) input);
-	struct Header *h1 = (struct Header*)&l;
+	struct Header *h1 = (struct Header*)input;
 	struct Header h = *h1;
+	int len = 0;
 
-	int len = h.len;
-	len &= ((1<<20)-1);
+	FOR(i, 0, 10)
+		len |= (h.TenSuff[i].len << (i*2));
 
-	output[0] = GetChar(h.a1);
-	output[1] = GetChar(h.a2);
-	output[2] = GetChar(h.a3);
-	output[3] = GetChar(h.a4);
-	output[4] = GetChar(h.a5);
-	output[5] = GetChar(h.a6);
-	output[6] = GetChar(h.a7);
-	output[7] = GetChar(h.a8);
+	*outputLength = len;
 
-	char *ptr = (char*)((char*)output +sizeof(long long));
+	int idx = 0;
 
-	for(int i = 0; i < (len - 8); )
+	FOR(i, 0, 10)
+	{
+		output[6 * i + 0] = GetChar(h.TenSuff[i].a11);
+		if(++idx >= len) break;
+		output[6 * i + 1] = GetChar(h.TenSuff[i].a12);
+		if(++idx >= len) break;
+		output[6 * i + 2] = GetChar(h.TenSuff[i].a13);
+		if(++idx >= len) break;
+		output[6 * i + 3] = GetChar(h.TenSuff[i].a14);
+		if(++idx >= len) break;
+		output[6 * i + 4] = GetChar(h.TenSuff[i].a15);
+		if(++idx >= len) break;
+		output[6 * i + 5] = GetChar(h.TenSuff[i].a16);
+		if(++idx >= len) break;
+	}
+
+	char *ptr = (char*)((char*)output + 60);
+
+	for(int i = 0; i < (len - 60); )
 	{
 		int k = i/6;
-		int tmp = input[k+2];
+		int tmp = input[k+10];
 
-		for(int j = 0; j < 6 &&  i < (len - 8); ++j)
+		for(int j = 0; j < 6 &&  i < (len - 60); ++j)
 		{
 			int idx = (tmp >> (5*j)) & 0x1F;
 			ptr[i++] = GetChar(idx);
